e55f68977bbb124e3dde162c433b7a141d46d67a
KalColeman <kalijahcoleman@icloud.com>
2025-11-17 21:46:17 +0000

    Basic task a implementation

---
+++ my-app/src/pages/Discover.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Discover () {
+    return(
+        <div>
+            <h1>Discover Something New!</h1>
+        </div>
+    );
+}
---
+++ my-app/src/pages/Pantry.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Pantry () {
+    return (
+        <div>
+            <h1>What are you wanting to cook with?</h1>
+        </div>
+    );
+}
